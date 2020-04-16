28d61dc1f4a5a2abacf1875bf0b7c2a9f87e3bb3
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-16 20:20:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ function addUpTo(array, index)
 
   } else {
     var currentIndex = array.length - 1;
-    
     console.log(array[currentIndex]);
+    
     return array[currentIndex] + addUpTo(array.slice(0, currentIndex));
 
   }
