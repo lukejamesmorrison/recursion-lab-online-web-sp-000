4efbef220aa30c3af86a7278eda1db6ce222bf29
Luke Morrison <lukejamesmorrison@gmail.com>
2020-04-16 20:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,69 @@
-// Code your solution here!
+let string = "pizza";
+reversed = reverseString(string);
 
+isPalindrome('lol');
+
+function printString(string, start = 0, end = 1)
+{
+  var char = string.substring(start, end);
+  console.log(char);
+  
+  if(end < string.length)
+  {
+    printString(string, end, end+1);
+  } else {
+    return;
+  }
+}
+
+function reverseString(string, reversedString = ''){
+  
+  if(string.length >= 1)
+  {
+    reversedString += string.slice(string.length - 1);
+    return reverseString(
+      string.substring(0, string.length - 1), 
+      reversedString
+    );
+  } else {
+    return reversedString;
+  }
+}
+
+function isPalindrome(str, index = 0) {
+
+  var frontIndex = index;
+  var rearIndex = str.length - 1 - index;
+  
+  if (frontIndex == rearIndex || rearIndex - frontIndex == 1) {
+    return true;
+  } else {
+    if (str.slice(frontIndex, frontIndex + 1) === str.slice(rearIndex, rearIndex + 1)) {
+      return isPalindrome(str, index + 1);
+    } else {
+      return false;
+    }
+  }
+}
+
+
+addUpTo([1, 4, 5, 3], 2);
+
+function addUpTo(array, index)
+{
+  if(array.length > index + 1)
+  {
+    array = array.slice(0, index+1);
+  }
+  
+  console.log(array);
+  
+  
+  if(array.length > 0)
+  {
+    console.log(array[0]);
+    return array[0] + addUpTo(array.slice(0, array.length));
+  } else {
+    return 0;
+  }
+}
